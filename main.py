82117ac227a4b19351b38303d1e3b7f5e372ef20
mahak-studio <mahourmahak77@gmail.com>
2026-09-01 07:33:54 -0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 # time: used to introduce delays between characters using time.sleep() to create the typing effect
 
 
-text = "✨ WELCOME TO PYTHON MAGICE ✨ \n Hello! This is a live typing animation effect in pure Python."
+text = "✨ WELCOME TO PYTHON MAGIC✨ \n Hello! This is a live typing animation effect in pure Python."
 for char in text:
   sys.stdout.write(char)   # Prints each character one by one without adding a newline
   sys.stdout.flush()       # Forces the console to display the character immediately, enabling the animation 
